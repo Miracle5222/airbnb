486ee0d47aa68c271137503d862c4337979a87fc
Roneil Bansas <Roneilbansas5222@gmail.com>
2021-08-30 01:37:50 -0700

    second commit, adding search date components, and card components

--- src/home.js
+++ src/home.js
@@ -1,12 +1,53 @@
 import React from 'react'
 import './home.css';
 import Banner from './banner';
-
+import Card from './card';
 function Home() {
     return (
         <div className='home'>
-       <h1>home component</h1>
-       <Banner />
+            <Banner />
+            <div className='home__section'>
+                 <Card
+                    src="https://images.pexels.com/photos/276720/pexels-photo-276720.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    title="Online Experience"
+                    description="Unique activities we can do together,
+                    led by a world of hosts."
+                />
+                <Card
+                    src="https://images.pexels.com/photos/3316923/pexels-photo-3316923.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    title="Online Experience"
+                    description="Spaces that are more than just a place to sleep."
+                />
+                <Card
+                    src="https://images.pexels.com/photos/706137/pexels-photo-706137.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    title="Online Experience"
+                    description="Unique activities we can do together,
+                    led by a world of hosts."
+                />
+              
+            </div>
+            <div className='home__section'>
+                <Card
+                    src="https://images.pexels.com/photos/5824519/pexels-photo-5824519.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    title="Online Experience"
+                    description="Unique activities we can do together,
+                    led by a world of hosts."
+                    price="$400/night"
+                />
+                <Card
+                    src="https://images.pexels.com/photos/6758788/pexels-photo-6758788.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    title="Online Experience"
+                    description="Spaces that are more than just a place to sleep."
+                    price="$350/night"
+                />
+                <Card
+                    src="https://images.pexels.com/photos/6585751/pexels-photo-6585751.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
+                    title="Online Experience"
+                    description="Unique activities we can do together,
+                    led by a world of hosts."
+                    price="$450/night"
+                />
+            </div>
         </div>
     )
 }
---
+++ src/search.js
@@ -0,0 +1,46 @@
+import React,{useState} from 'react'
+import './search.css';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
+import { DateRangePicker } from 'react-date-range';
+import { Button } from '@material-ui/core';
+import PeopleIcon from '@material-ui/icons/People';
+
+
+function Search() {
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+
+    const SelectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection",
+    };
+
+    function handleSelect(ranges) {
+        setStartDate(ranges.selection.startDate);
+        setEndDate(ranges.selection.endDate);
+    }
+
+    return (
+        <div className='search'>
+            <DateRangePicker
+                ranges={[SelectionRange]}
+                onChange={handleSelect}
+            />
+            <h2>
+                Number of guests
+                <PeopleIcon/>
+            </h2>
+            <input min={0}
+                defaultValue={2}
+                type="number"
+            />
+            <Button>Search Airbnb</Button>
+        </div>
+    )
+}
+
+export default Search;
